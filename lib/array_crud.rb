434ed88d1de7858cf95bac8c511e91dc8c1e82de
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-02 17:26:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array.select[index_number]
-  return array[index_number]
+  return [index_number]
 end
 
 def retrieve_first_element_from_array(array)
